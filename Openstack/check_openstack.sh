7b7055643ddad7e81f74cbb0dab0f27215dbad68
seawind <mpf.hgsf@gmail.com>
2015-01-21 17:41:17 +0800

    add zabbix zbx_export_templates_Openstack.xml

--- Openstack/check_openstack.sh
+++ Openstack/check_openstack.sh
@@ -8,12 +8,12 @@ log=/tmp/check_openstack.log
 tmp=/tmp/openstack.tmp
 time=`date +%M`
 [ ! -s $log ] && /usr/bin/openstack-status 2>&1 |egrep -v 'disabled on boot|=|not sourced' >$log
-[ ! -s $tmp  -o $((time%30)) -eq 10 ] && /usr/bin/openstack-status 2>&1 |egrep -v 'disabled on boot|=|not sourced' >$tmp
+[ ! -s $tmp  -o $time -lt 10 ] && /usr/bin/openstack-status 2>&1 |egrep -v 'disabled on boot|=|not sourced' >$tmp
 
 
 get_status(){
-        h_srv=`cat $log|grep -i $1|awk '{print $NF}' 2>/dev/null`
-	n_srv=`cat $tmp|grep -i $1|awk '{print $NF}' 2>/dev/null`
+        h_srv=`cat $log|grep -iw "^$1:"|awk '{print $NF}' 2>/dev/null`
+	n_srv=`cat $tmp|grep -iw "^$1:"|awk '{print $NF}' 2>/dev/null`
 	if [ "$n_srv" == "active" -a "$h_srv" == "active" ]; then
 	    check_srv=0
 	elif [ -n "$n_srv"  -o -n "h_srv" ];then
