748bb03fe3fe0cb16d295a64b0bd4689b38d4991
seawind <mpf.hgsf@gmail.com>
2015-05-19 23:40:01 +0800

    update Test_ssddisk.sh

--- benchmark/Test_ssddisk.sh
+++ benchmark/Test_ssddisk.sh
@@ -3,30 +3,31 @@ time=`date +%s`
 mkdir -p /tmp/ssdtest/
 log=/tmp/ssdtest/dd_test_$time.log
 ramlog=/tmp/ssdtest/ramlog_dd_test_$time.log
-
+#################
 size=40000000
 ssddev=/dev/sdb1
-mountpoint=/data1
+mountpoint=/data
+################
 i=1
 for bs in 4k 64k 128k 1024k 2048k 4096k;do
-    echo "###$i Write bs=$bs" >> $log
+    #echo "###$i Write bs=$bs" >> $log
     bksize=`echo $bs|sed 's/k//g'`
     coun=`echo $size/$bksize|bc`
-    echo "### write bs=$bs bksize=$bksize count=$coun" >> $log
+    echo "###$i write bs=$bs count=$coun" >> $log
     echo 3 > /proc/sys/vm/drop_caches
-    dd if=/dev/zero of=/data1/40gssd_${bs} bs=$bs count=$coun oflag=direct 2>>$log 
+    dd if=/dev/zero of=${mountpoint}/40gssd_${bs} bs=$bs count=$coun oflag=direct 2>>$log 
     sleep 3
     i=$((i+1))
 done
 
 #echo 1 > /proc/sys/vm/drop_caches; echo 0 > /proc/sys/vm/drop_caches;
 for bs in 4k 64k 128k 1024k 2048k 4096k;do
-    echo "###$i Read test bs=$bs=========" >> $log
+    #echo "###$i Read test bs=$bs=========" >> $log
     bksize=`echo $bs|sed 's/k//g'`
     coun=`echo $size/$bksize|bc`
-    echo "###read bs=$bs count=$coun" >> $log
+    echo "###$i read bs=$bs count=$coun" >> $log
     echo 3 > /proc/sys/vm/drop_caches
-    dd if=/data1/40gssd_${bs}  of=/dev/null bs=$bs count=$coun iflag=direct 2>>$log
+    dd if=${mountpoint}/40gssd_${bs}  of=/dev/null bs=$bs count=$coun iflag=direct 2>>$log
 
     i=$((i+1))
     echo "###$i Ramdev read bs=$bs=========" >> $ramlog
